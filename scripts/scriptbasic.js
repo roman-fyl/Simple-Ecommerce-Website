076f6e735026e517b8e49be6730b51206ea75036
Roman <roman.if.ko@gmail.com>
2023-11-01 23:37:28 -0500

    upd

--- scripts/scriptbasic.js
+++ scripts/scriptbasic.js
@@ -1,31 +1,33 @@
 "use strict";
 
 const menuSwitcher = () => {
- 
+
   const navigation = document.querySelector(".header__content nav");
   const menu = document.querySelector(".header__mobile");
   const elementNavMenu = document.querySelectorAll('.element-menu');
 
- 
-const menuToggle = () => {
- if (navigation) {
-  navigation.classList.toggle('elementShow');
-  return
- }
-}
-if (elementNavMenu) {
-  if (Array.isArray(elementNavMenu)) {
-    elementNavMenu.forEach((element) => {
-      element.addEventListener('click', (event) => {
-        elementNavMenu.forEach((item) => {
-          item.classList.remove('elementActive');
-        });
-        event.target.classList.add('elementActive');
-      });
-    });
-  }
-}
+  const menuToggle = () => {
+    
+    if (navigation) {
+      navigation.classList.toggle('elementShow');
 
+      return
+    }
+  }
+
+  if (elementNavMenu) {
+    if (Array.isArray(elementNavMenu)) {
+      
+      elementNavMenu.forEach((element) => {
+        element.addEventListener('click', (event) => {
+          elementNavMenu.forEach((item) => {
+            item.classList.remove('elementActive');
+          });
+          event.target.classList.add('elementActive');
+        });
+      });
+    }
+  }
 
   menu.addEventListener('click', menuToggle);
 };
